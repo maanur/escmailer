3712b5d8bff149d4d7694a0c187cd1d89b896770
Максим Грушин < >
2017-02-17 19:30:03 +0300

    001

--- test1.go
+++ test1.go
@@ -1,33 +1,19 @@
 package main
 
-/*Это будет библиотечка COS-запросов к АСУЛОНу*/
+/* Начну с отправки тествого сообщения */
 
 import (
 	"fmt"
 	"log"
 	"net"
-
-	"github.com/Cristofori/kmud/telnet"
+	"net/smtp"
+	"gnd.la/net/mail"
 )
 
 func main() {
-	_, err := net.ResolveTCPAddr("tcp", "127.0.0.1:23")
-	if err != nil {
-		log.Fatal(err)
-	}
-	conn, err := net.Dial("tcp", "127.0.0.1:23")
-	if err == nil {
-		fmt.Println("Connected...")
-	} else {
-		log.Fatal(err)
-	}
-	cache := telnet.NewTelnet(conn)
-	for i := 0; i < 2; i++ {
-		func() {
-			what := make([]byte, 256)
-			_, err = cache.Read(what)
-			fmt.Println(string(what))
-		}()
-	}
+	log.Fatal("Not yet")
+}
 
+func Start {
+	
 }
