e95801ec241713af58007cec154e4c5fcb13572b
Максим Грушин < >
2017-02-19 09:16:46 +0300

    zip

--- test1.go
+++ test1.go
@@ -3,11 +3,15 @@ package main
 /* Начну с отправки тествого сообщения */
 
 import (
+	"archive/zip"
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"os"
+
+	"io/ioutil"
 
 	"github.com/jpoehls/gophermail"
 )
@@ -16,6 +20,7 @@ func main() {
 	m := readConf()
 	fmt.Println(m)
 	log.Fatal("Not yet")
+	pack([]string{"config.conf"})
 }
 
 type escMsg struct {
@@ -81,3 +86,65 @@ func readConf() (m *escMsg) {
 	}
 	return m
 }
+
+func pack(files []string) string {
+	buf := new(bytes.Buffer)
+	arch := zip.NewWriter(buf)
+	for _, file := range files {
+		f, err := arch.Create(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = f.Write(readFile(file))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	var fm os.FileMode
+	err := ioutil.WriteFile("test.zip", readFile(files[0]), fm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return "test.zip"
+}
+func readFile(f string) []byte {
+	o, err := ioutil.ReadFile(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return o
+}
+
+/*
+// Create a buffer to write our archive to.
+buf := new(bytes.Buffer)
+
+// Create a new zip archive.
+w := zip.NewWriter(buf)
+
+// Add some files to the archive.
+var files = []struct {
+    Name, Body string
+}{
+    {"readme.txt", "This archive contains some text files."},
+    {"gopher.txt", "Gopher names:\nGeorge\nGeoffrey\nGonzo"},
+    {"todo.txt", "Get animal handling licence.\nWrite more examples."},
+}
+for _, file := range files {
+    f, err := w.Create(file.Name)
+    if err != nil {
+        log.Fatal(err)
+    }
+    _, err = f.Write([]byte(file.Body))
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+// Make sure to check the error on Close.
+err := w.Close()
+if err != nil {
+    log.Fatal(err)
+}
+*/
